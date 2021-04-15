779c9b36b8d8bcd3804c07fd0770466b149caa2d
yuan xiao wei <yxw159hh@163.com>
2021-04-15 15:23:46 +0800

    css修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from './App.moudle.scss'
+import styles from './App.module.scss'
 
 const Apps:React.FC = () =>{
     return(
-        <div>yuan xiao wei</div>
+        <div className={styles.nameClass}>yuan xiao wei</div>
     )
 }
 export default Apps
